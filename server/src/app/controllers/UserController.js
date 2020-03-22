34f8a5b2215453876e1ab5333d1284278dd0665f
Leonardo Ferreira Alves <leonardofa@gmail.com>
2020-03-22 20:54:25 -0300

    E-mail duplicado

--- server/src/app/controllers/UserController.js
+++ server/src/app/controllers/UserController.js
@@ -2,9 +2,13 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    if (req.body.email === 'leonardo.fa@gmail.com') {
-      return res.status(400).json({ error: 'email not allowed' });
+    const { email } = req.body;
+
+    const checkEmail = await User.findOne({ where: { email } });
+    if (checkEmail) {
+      return res.status(400).json({ error: 'Duplicated email' });
     }
+
     const user = await User.create(req.body);
 
     return res.json(user);
